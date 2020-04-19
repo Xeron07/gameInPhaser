60b83420056f37f828bf41d2d289cd85306db934
xeron07 <ni.xeron07@gmail.com>
2020-04-19 21:51:19 +0600

    scene bg img added

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let game = null;
 let config = {
-  width: 800,
-  height: 600,
+  width: 467,
+  height: 350,
   backgroundColor: 0x000000,
   marginLeft: 250,
   scene: [Scene1, Scene2],
--- scene2.js
+++ scene2.js
@@ -3,10 +3,17 @@ class Scene2 extends Phaser.Scene {
     super("gamePage");
   }
 
+  preload() {
+    this.load.image("bg1", "ext/bg1.jpg");
+  }
+
   create() {
+    this.background = this.add.image(0, 0, "bg1");
+    this.background.setOrigin(0, 0);
     this.add.text(20, 20, "In game scene....", {
       font: "25px",
-      fill: "yellow",
+      fill: "blue",
+      left: 200,
     });
   }
 }
